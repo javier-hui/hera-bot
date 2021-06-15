b42a6478521571de3aed29cb53e4195d9a0dc150
javier <javier.hui@gmail.com>
2021-06-15 21:29:16 +0800

    pool test

--- commands/show.js
+++ commands/show.js
@@ -21,7 +21,7 @@ cmd = async (client, message, args) => {
             break;
         case 'din':
         case 'dinner':
-
+            const query = `SELECT DISTINCT name, at_home, reason FROM supper WHERE weekday = ${(new Date).getDay()} AND dinner = TRUE;`;
             const res = loadDB(query);
         /*
             await dbClient.connect();
