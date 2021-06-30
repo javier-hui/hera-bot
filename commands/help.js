45dedb1aec42cd8067f4cfafd97c1305fa27f38f
javier <javier.hui@gmail.com>
2021-06-30 18:13:32 +0800

    updated help

--- commands/help.js
+++ commands/help.js
@@ -21,11 +21,11 @@ cmd = async (client, message) => {
                 value: "shows whether the person would eat at home or not for this week. shows yourself by default.\ne.g. `hera show javier`"
             },
             {
-                name: "hera home `lunch/dinner` all `weekday` (`reason`)",
-                value: "rewrites the rule for that weekday regularly, if not home pls provide reason\ne.g. `hera no dinner all friday (party)`"
+                name: "hera `home/no` `lunch/dinner` all `weekday` (`reason`)",
+                value: "rewrites the rule for that weekday regularly, if not home then provide reason\ne.g. `hera no dinner all friday (party)`"
             },
             {
-                name: "hera [home/no] [lunch/dinner] [dd/mm] (reason)",
+                name: "hera `home/no` `lunch/dinner` `dd/mm` (`reason`)",
                 value: "sets a specific rule for that day in particular\ne.g. `hera home lunch 13/2` will update your availability for lunch on 13/2\ne.g. `hera no dinner tonight (eat with colleagues)` say this when you can't come home tonight"
             },
             {
